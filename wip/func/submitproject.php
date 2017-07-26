3b942334085006cc7fd8808d7b8ad23b0db6a9ce
John Nitzsche <nitzschejohn@gmail.com>
2017-07-26 21:05:17 +0200

    Formular-Action Issue: Post ist leer, eine Datei von BKU fehlt!!!

--- wip/func/submitproject.php
+++ wip/func/submitproject.php
@@ -2,13 +2,37 @@
 //require für Datenbankverbindungseinstellungen
 require_once(dirname(__DIR__) . '/globalconfig.php');
 
+print_r($HTTP_POST_VARS);
+
 if (!empty($_POST)) {
 
     $pdo = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name, $db_user, $db_pass);
     $pdo->exec("set names utf8");
     //$sql = "SELECT * FROM " . $db_pref . "_alles WHERE strecke=" . $_GET['strecke'] . " ORDER BY km_i ASC";
 
-    $sql = "INSERT INTO ".$db_pref."_projekte (ID, projektname, typ, ort, ortgeo, ansprechpartner, start, ende, zusatz, erstellt, erstelltvon) VALUES ";
+    $sql = "INSERT INTO ".$db_pref."_projekte (projektname, ortgeo, ansprechpartner, projektleiter, start, ende, zusatz, erstelltvon, benutzer, status) VALUES (";
+    $sql += $_POST['fin-name'];
+    $sql += ",";
+    $sql += serialize($_POST['fin-orte']);
+    $sql += ",";
+    $sql += $_POST['fin-ansprechpartner'];
+    $sql += ",";
+    $sql += $_POST['fin-projektleiter'];
+    $sql += ",";
+    $sql += $_POST['fin-start'];
+    $sql += ",";
+    $sql += $_POST['fin-ende'];
+    $sql += ",";
+    $sql += $_POST['fin-zusatz'];
+    $sql += ",";
+    $sql += $_SESSION['user']['userid'];
+    $sql += ",";
+    $sql += serialize($_POST['fin-benutzerids']);
+    $sql += ",";
+    $sql += $_POST['fin-statusnr'];
+    $sql += ");";
+
+    $pdo->query($sql);
 
 
 
@@ -17,6 +41,6 @@ if (!empty($_POST)) {
 } else {
     //$message['notice'] = 'Geben Sie Ihre Zugangsdaten ein um sich anzumelden.<br />' .
     //    'Wenn Sie noch kein Konto haben, gehen Sie <a href="./register.php">zur Registrierung</a>.';
-    header('Location: ../createproject.php');
+   echo 'leer';
 }
 ?>
--- wip/inc/project_step4.php
+++ wip/inc/project_step4.php
@@ -2,79 +2,79 @@
 <form class="form-horizontal form-label-left" id="form-fin" action="func/submitproject.php" method = "post">
 
     <div class="form-group">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="fin-name">Projektname
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="id-fin-name">Projektname
             <span class="required">*</span>
         </label>
         <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="text" id="fin-name" name="fin-name"
+            <input type="text" id="id-fin-name" name="fin-name"
                    class="form-control col-md-7 col-xs-12" disabled>
         </div>
     </div>
 
     <div class="form-group">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="fin-ansprechpartner">Ansprechpartner
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="id-fin-ansprechpartner">Ansprechpartner
             <span class="required">*</span>
         </label>
         <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="text" id="fin-ansprechpartner" name="fin-ansprechpartner"
+            <input type="text" id="id-fin-ansprechpartner" name="fin-ansprechpartner"
                    class="form-control col-md-7 col-xs-12" disabled>
         </div>
     </div>
 
     <div class="form-group">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="fin-projektleiter">Projektleiter
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="id-fin-projektleiter">Projektleiter
         </label>
         <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="text" id="fin-projektleiter" name="fin-projektleiter"
+            <input type="text" id="id-fin-projektleiter" name="fin-projektleiter"
                    class="form-control col-md-7 col-xs-12" disabled>
         </div>
     </div>
 
     <div class="form-group">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="fin-start">Startdatum
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="id-fin-start">Startdatum
             <span class="required">*</span>
         </label>
         <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="text" id="fin-start" id="fin-name"
+            <input type="text" id="id-fin-start" name="fin-name"
                    class="form-control col-md-7 col-xs-12" disabled>
         </div>
     </div>
 
     <div class="form-group">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="fin-ende">Enddatum
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="id-fin-ende">Enddatum
             <span class="required">*</span>
         </label>
         <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="text" id="fin-ende" name="fin-ende"
+            <input type="text" id="id-fin-ende" name="fin-ende"
                    class="form-control col-md-7 col-xs-12" disabled>
         </div>
     </div>
 
 
     <div class="form-group">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="fin-status">Status
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="id-fin-status">Status
             <span class="required">*</span>
         </label>
         <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="text" id="fin-status"  name="fin-status"
+            <input type="text" id="id-fin-status"  name="fin-status"
                    class="form-control col-md-7 col-xs-12" disabled>
         </div>
     </div>
 
     <div class="form-group">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="fin-benutzer">Benutzer
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="id-fin-benutzer">Benutzer
         </label>
         <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="text" id="fin-benutzer" name="fin-benutzer"
+            <input type="text" id="id-fin-benutzer" name="fin-benutzer"
                    class="form-control col-md-7 col-xs-12" disabled>
         </div>
     </div>
 
     <div class="form-group">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="fin-zusatz">Zusatzinformationen
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="id-fin-zusatz">Zusatzinformationen
         </label>
         <div class="col-md-6 col-sm-6 col-xs-12">
-            <TEXTAREA form="form-fin" id="fin-zusatz" name="fin-zusatz" class="form-control col-md-7 col-xs-12" rows="20" disabled></TEXTAREA>
+            <TEXTAREA form="form-fin" id="id-fin-zusatz" name="fin-zusatz" class="form-control col-md-7 col-xs-12" rows="20" disabled></TEXTAREA>
         </div>
     </div>
 
@@ -82,29 +82,29 @@
     <h3>Maschinelle Informationen zur Weiterverarbeitung</h3>
 
     <div class="form-group">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="fin-statusnr">Statusnr
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="id-fin-statusnr">Statusnr
         </label>
         <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="text" id="fin-statusnr"  name="fin-statusnr"
+            <input type="text" id="id-fin-statusnr"  name="fin-statusnr"
                    class="form-control col-md-7 col-xs-12" disabled>
         </div>
     </div>
 
     <div class="form-group">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="fin-benutzerids">Benutzer-IDS
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="id-fin-benutzerids">Benutzer-IDS
         </label>
         <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="text" id="fin-benutzerids" name="fin-benutzerids"
+            <input type="text" id="id-fin-benutzerids" name="fin-benutzerids"
                    class="form-control col-md-7 col-xs-12" disabled>
         </div>
     </div>
 
     <div class="form-group">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="fin-orte">Orte
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="id-fin-orte">Orte
             <span class="required">*</span>
         </label>
         <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="text" id="fin-orte" name="fin-orte"
+            <input type="text" id="id-fin-orte" name="fin-orte"
                    class="form-control col-md-7 col-xs-12" disabled>
         </div>
     </div>
@@ -112,7 +112,7 @@
     <br/>
     <br/>
 
-    <button class="btn btn-success col-lg-12 col-md-12 col-xs-12" type="submit" name="projekterstellen"
+    <button id="submitbutton" class="btn btn-success col-lg-12 col-md-12 col-xs-12" type="submit"
             disabled="disabled">Projekt erstellen
     </button>
 </form>
@@ -137,66 +137,66 @@
         var finish = true;
 
 
-        document.getElementById('fin-projektleiter').value = document.getElementById('project_projektleiter').value;
-        document.getElementById('fin-ende').value = document.getElementById('project_ende').value;
+        document.getElementById('id-fin-projektleiter').value = document.getElementById('project_projektleiter').value;
+        document.getElementById('id-fin-ende').value = document.getElementById('project_ende').value;
 
-        document.getElementById('fin-statusnr').value = $('input[name="project_status"]:checked').val()
+        document.getElementById('id-fin-statusnr').value = $('input[name="project_status"]:checked').val()
 
-        document.getElementById('fin-benutzer').value = arraybenutzernamen;
-        document.getElementById('fin-benutzerids').value = arraybenutzerids;
+        document.getElementById('id-fin-benutzer').value = arraybenutzernamen;
+        document.getElementById('id-fin-benutzerids').value = arraybenutzerids;
 
-        document.getElementById('fin-zusatz').value = document.getElementById('project_zusatz').value;
+        document.getElementById('id-fin-zusatz').value = document.getElementById('project_zusatz').value;
 
 
         if (document.getElementById('project_name').value == "") {
-            zeigeunvollständig('fin-name');
+            zeigeunvollständig('id-fin-name');
         }
         else {
-            document.getElementById('fin-name').value = document.getElementById('project_name').value;
-            resetstyle('fin-name');
+            document.getElementById('id-fin-name').value = document.getElementById('project_name').value;
+            resetstyle('id-fin-name');
         }
 
         if (document.getElementById('project_ansprechpartner').value == "") {
-            zeigeunvollständig('fin-ansprechpartner');
+            zeigeunvollständig('id-fin-ansprechpartner');
         }
         else {
-            document.getElementById('fin-ansprechpartner').value = document.getElementById('project_ansprechpartner').value;
-            resetstyle('fin-ansprechpartner');
+            document.getElementById('id-fin-ansprechpartner').value = document.getElementById('project_ansprechpartner').value;
+            resetstyle('id-fin-ansprechpartner');
         }
 
         if (document.getElementById('project_start').value == "") {
-            zeigeunvollständig('fin-start');
+            zeigeunvollständig('id-fin-start');
         }
         else {
-            document.getElementById('fin-start').value = document.getElementById('project_start').value;
-            resetstyle('fin-start');
+            document.getElementById('id-fin-start').value = document.getElementById('project_start').value;
+            resetstyle('id-fin-start');
         }
 
         if (arrayorte == "") {
-            zeigeunvollständig('fin-orte');
-            document.getElementById('fin-orte').value = "Sie müssen mindestens einen Ort angeben!";
+            zeigeunvollständig('id-fin-orte');
+            document.getElementById('id-fin-orte').value = "Sie müssen mindestens einen Ort angeben!";
             finish = false;
         }
         else {
-            document.getElementById('fin-orte').value = arrayorte;
-            resetstyle('fin-orte');
+            document.getElementById('id-fin-orte').value = arrayorte;
+            resetstyle('id-fin-orte');
         }
 
         switch ($('input[name="project_status"]:checked').val()) {
             case '0':
-                document.getElementById('fin-status').value = "In Bearbeitung";
+                document.getElementById('id-fin-status').value = "In Bearbeitung";
                 break;
             case '1':
-                document.getElementById('fin-status').value = "abgeschlossen";
+                document.getElementById('id-fin-status').value = "abgeschlossen";
                 break;
             case '2':
-                document.getElementById('fin-status').value = "verzögert";
+                document.getElementById('id-fin-status').value = "verzögert";
                 break;
             case '3':
-                document.getElementById('fin-status').value = "Es gibt Probleme";
+                document.getElementById('id-fin-status').value = "Es gibt Probleme";
                 break;
             default:
-                document.getElementById('fin-status').value = "?";
+                document.getElementById('id-fin-status').value = "?";
         }
 
 
@@ -204,8 +204,11 @@
 
         //console.log(finish);
 
-        if (!finish) {
-
+        if (finish) {
+            document.getElementById("submitbutton").disabled = false;
+        } else
+        {
+            document.getElementById("submitbutton").disabled = true;
         }
 
 
